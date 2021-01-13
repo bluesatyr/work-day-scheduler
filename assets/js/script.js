77b3181e0a9fef079c683ed91b77cad8e0dec809
bluesatyr <shawnevans.music@gmail.com>
2021-01-12 21:19:36 -0500

    initial logic

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var now = moment();
+console.log(now.hour(20));
+
+var currentHour = now.format("hA");
+console.log(currentHour);
+
+// Load tasks from localStorage
+// Render the tasks in the grid => renderTasks()
+// get the current hour
+// compare to time slots on schedule
+// for each hour, check to see if the current hour is < = or >
+// if (timeSlot < currentHour) {addClass("past")}
+// if (timeSlot === currentHour) {addClass("current")}
+// if (timeSlot > currentHour) {addClass("future")}
+// when field has focus make text input and back to text field when out of focus
+// save task to localStorage
+// update the view => renderTasks()
+
+var thisHour = $('.am-9').text().trim(); // use data-* and dataset.*
+console.log(thisHour);
+$('.am-9').next().addClass('present'); // add present class to the next sibling
+
+
+if (currentHour === thisHour){
+    console.log(true);
+} else {
+    console.log(false);
+}
