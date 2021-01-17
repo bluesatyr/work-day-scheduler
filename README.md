21b2f9266f825e3f204e58ee238dc7725e22c06b
bluesatyr <shawnevans.music@gmail.com>
2021-01-17 14:57:12 -0500

    add task entry and local storage of tasks array

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# work-day-scheduler
-A simple calendar application that allows a user to save events for each hour of the day.
+# Work Day Scheduler
+
+
+## Description
+A simple task scheduling application that allows a user to save events for each hour of the work day. Once saved by clicking the blue button, tasks are saved to localStorage for retrieval the next time the application is loaded.
+
+## Screenshot
+
+![Final site](./assets/images/screenshot.png)
+
+## Deployed at:
+
+https://bluesatyr.github.io/work-day-scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,20 +8,36 @@ var currentHour = now.format("hA");
 console.log(currentHour);
 
 // global tasks array
-var tasks = ["task 1", "task 2", "task 3", "task 4", "task 5", "task 6", "task 7", "task 8", "task 9"];
+var tasks = ["", "", "", "", "", "", "", "", ""];
 
-// Load tasks from localStorage
-var getTasks = function() {
-    var loadedtasks = JSON.parse(localStorage.getItem('tasks'));
-    return loadedtasks;
-};
+// render current date to the header
+var currentDate = function () {
+    var today = now.format('dddd, LL')
+    $('#currentDay').text(today);
+}
+
 
 // render tasks to .hour elements in the DOM
 var renderTasks = function() {
     $('.hour').each(function(i) {
-        $(this).text(tasks[i]);
+        var text = tasks[i];
+        var taskP = $("<p>")
+            .text(text);
+        $(this).append(taskP);
         console.log(this);
     });
+};
+
+// Load tasks from localStorage
+var getTasks = function() {
+    var loadedtasks = JSON.parse(localStorage.getItem('tasks'));
+    if (loadedtasks) {
+        tasks = loadedtasks;
+        renderTasks();
+    } else {
+        renderTasks();
+    }
+    
 };
 
 // save current tasks to storage
@@ -29,34 +45,47 @@ var tasksToStorage =  function() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
+// when user clicks save button update tasks array and save to localStorage
 $('.save-btn').click(function() {
     $('.hour').each(function(i){
-        tasks[i] = $(this).text();
+        var taskP = $(this).find('p');
+        tasks[i] = taskP.text().trim();
         console.log(tasks);
         tasksToStorage();
     })
 });
 
-/* save input task to global tasks array
-var saveTasks = function() {
-    $('.hour').each(function(i){
-        tasks[i] = $(this).text();
-        console.log(tasks);
-        tasksToStorage();
-    })
-}
-*/
+// when task field gets focus/is clicked, turn into input area
+$(".hour").on("click", function() {
+    // get current task from p 
+    var textP = $(this).find('p');
+    var text = textP.text().trim();
+    console.log(text);
+    
+    var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+    
+    textP.replaceWith(textInput);
+    textInput.trigger("focus");
+}); 
 
-
-
- /*   
-    function(){
-    var index =  $('.save-btn').index(this);
-    var taskText = $(this).prev().text();
-    tasks[index] = taskText;
+// on blur return to a paragraph element
+$(".hour").on("blur", "textarea", function() {
+    // get the textarea's current value/text
+    var text = $(this)
+        .val()
+        .trim();
+    
+    
+    // recreate p element
+    var taskP = $("<p>")
+        .text(text);
+    
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
+    
 });
-*/
-
 
 // compare to time slots on schedule to current hour & set proper class
 var hourStatus =function(){
@@ -74,7 +103,10 @@ var hourStatus =function(){
     });
 };
 
+
+$( document ).ready(currentDate);
 $( document ).ready(hourStatus);
+$( document ).ready(getTasks);
 var checkHour = setInterval(hourStatus, 60000);
 
 
